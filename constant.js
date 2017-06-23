af8b97820e05f243954bb79ac4fa0258abfb105f
shinmarq <shin@chatbot.ph>
2017-06-23 12:10:14 +0800

    sg key

--- constant.js
+++ constant.js
@@ -4,6 +4,6 @@ module.exports.WIT_TOKEN = 'HREB4NU5ICGSWNJB7TVL7AAN4SA3IEH3';
 module.exports.MICROSOFT_APP_ID = '30798881-38ff-4008-b027-165467700493';
 module.exports.MICROSOFT_APP_PASSWORD = 'rbFgcy0xo3MnX1khFs2Aj9g';
 
-module.exports.SG_TOKEN = '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';
+module.exports.SG_TOKEN = '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';
 
 module.exports.GET_STARTED_URL = 'https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAa06qzNsjUBAGxzgTCbRHBrGC9r8x82RD4jrPjD0nffTHJZBnDasZBmqtiCa5OIBZAiZB3gGzHOvJY0RcsQD2PZBesL5HazaZB5JZBZC1iujorV13xrcCCw9dM6kmcLvDmPQb24eJD71YwMn8pehN4ZCAqhUHNZBqi00sWEWk7AjBPAZDZD';
