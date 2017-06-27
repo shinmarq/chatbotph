4ea2f9b83004bd183c6cd4a25458028d28f00413
shinmarq <shin@chatbot.ph>
2017-06-27 13:50:30 +0800

    test

--- constant.js
+++ constant.js
@@ -4,4 +4,4 @@ module.exports.WIT_TOKEN = 'HREB4NU5ICGSWNJB7TVL7AAN4SA3IEH3';
 module.exports.MICROSOFT_APP_ID = '30798881-38ff-4008-b027-165467700493';
 module.exports.MICROSOFT_APP_PASSWORD = 'rbFgcy0xo3MnX1khFs2Aj9g';
 
-module.exports.GET_STARTED_URL = 'https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAa06qzNsjUBAGxzgTCbRHBrGC9r8x82RD4jrPjD0nffTHJZBnDasZBmqtiCa5OIBZAiZB3gGzHOvJY0RcsQD2PZBesL5HazaZB5JZBZC1iujorV13xrcCCw9dM6kmcLvDmPQb24eJD71YwMn8pehN4ZCAqhUHNZBqi00sWEWk7AjBPAZDZD';
+// module.exports.GET_STARTED_URL = 'https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAACZCTQNYC8QBAOtFIeWXEmFGL3bplZB9ojrjv4OzxGQc4tTskpHUt3cgu3i7cpX3xS9KhByXqCqf78ZCt8ZAzfngnJZC4eZCTHnX0wJYdy3utNX3XQ1ixEmsHldZCfc3TODK2R8n7nsxQVWQTL42ueqScyC6nI8MMEw7xFKH2LSgZDZD';
